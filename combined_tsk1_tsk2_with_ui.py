b969106a173764fc9862257f95d0208648271bf7
Mula Siva Prasad Babu <111627965+Siva-Mula-03@users.noreply.github.com>
2025-03-31 19:18:46 +0530

    Update combined_tsk1_tsk2_with_ui.py

--- combined_tsk1_tsk2_with_ui.py
+++ combined_tsk1_tsk2_with_ui.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import requests
 import pandas as pd
@@ -5,6 +6,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import re
 import os
+import base64
 
 # SEC Base URL
 BASE_URL = "https://www.sec.gov"
